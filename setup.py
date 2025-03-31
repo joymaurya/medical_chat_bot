a68bac2594b91f6f091d2bb787bb07dc980e55c2
joymaurya <95065785+joymaurya@users.noreply.github.com>
2025-03-31 15:58:35 +0530

    folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Med GEN AI",
+    version="0.0.0",
+    author="Joy Maurya",
+    author_email="joymaurya1800@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ template.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
+
+list_of_files=[
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "app.py",
+    "research/trails.ipynb"
+]
+
+for file in list_of_files:
+    file_path=Path(file)
+    filedir,filename=os.path.split(file_path)
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating Directory; {filedir} for the file:{filename}")
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path)==0):
+        with open(file_path,"w") as f:
+            pass
+        logging.info(f"Creating empty file: {file_path}")
+    else:
+        logging.info(f"{file_path} is already exists")
